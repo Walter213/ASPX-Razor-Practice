cd507ef17215cd838f89ce8c3799d08271bffa85
Walter213 <46505629+Walter213@users.noreply.github.com>
2019-01-11 11:49:55 -0700

    Start OOPs Review
    Jan 11 2019, Review creating a console applicatio. Create a new class called Die.

---
+++ OOPsReviewReal/OOPsReviewReal/Die.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReviewReal
+{
+    class Die
+    {
+        // Data Members
+        // Usually Private
+        private int _Side;
+        private string _Color;
+        // private int _FaceValue; Dont need because public int Facevalue { get; set; }
+
+        // Properties
+        // are responsible to assigning and retriving data to/from their associated data member
+        // retriving data from a data member uses the get{}
+        // assigning data to a data member uses the set{}
+        // properties ened to be exposed to outside users
+        // properties will have a return datatype BUT no parameter list
+
+        // Fully Implemented Property
+        // Has a defined Data Member that the developer can directly access.
+        public int Side
+        {
+            get
+            {
+                // Returns data of a specific datatype
+                return Side;
+            }
+
+            set
+            {
+                // Assigns a supplied value to the data member
+                // the supplied value is located in the key wird: value
+                // Optionally include data value validation to ensure an
+                //  apporopriate value has been supplied
+                if (value >= 6 && value <= 20)
+                {
+                    // This is a acceptable value to keep
+                    _Side = value;
+                }
+                else
+                {
+                    // This is an unacceptable value
+                    // Issue a user friendly error message
+                    throw new Exception("Die cannot be " + value.ToString() + " sided. Die must be between 6 and 20 sided.");
+                }              
+            }
+        }
+
+        // Auto Implemented Property
+        // NO Data Member definition
+        // The data member is internally created for you
+        // The data member datatype is take from your return datatype
+        //  specificed on the Property header
+        // Auto implemented properties are usually used when there is
+        //  no need to internal validation
+        // access to a value is managed by an auto implemented property
+        //  MUST be done via the property
+
+        public int Facevalue { get; set; }
+    }
+}
---
+++ OOPsReviewReal/OOPsReviewReal/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OOPsReviewReal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OOPsReviewReal")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce329649-3dcd-4d99-a5f1-58b021326ea6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
